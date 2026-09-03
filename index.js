d7f9d3812341599cc866b76bee590614a250b062
Whisper25 <whisper252552@gmail.com>
2026-09-03 06:56:03 +0200

    adding export & import

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
+import Article from "./Article.mjs";
+import { mult } from "./math.mjs";
+
 console.log('hi from node');
 const num1 = 5;
 const num2 = 8;
-const mult = (n1,n2)=> n1 * n2;
+
 console.log(num1+num2);
 console.log(mult(num1,num2));
-class Article{
-    constructor(content){
-        this.content=content;
-    }
-    render(){
-        return this.content
-    }
-}
+
 const newArticle = new Article('content');
 console.log(newArticle.render());
---
+++ math.mjs
@@ -0,0 +1,2 @@
+export const mult = (n1,n2)=> n1 * n2;
+export const sum = (n1,n2)=> n1 + n2;
